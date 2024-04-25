9b6f78ed63a8c89573e5cc39331256349bce5087
SwirX <swirxytb@gmail.com>
2024-04-25 23:51:53 +0100

    update

--- allanime.py
+++ allanime.py
@@ -1,39 +1,43 @@
+import os
 import json
+import time
 import requests
 
+class VideoLink:
+    def __init__(self, link="", hls=False, mp4=False, resolutionStr="", src="", rawUrls=None):
+        self.link = link
+        self.hls = hls
+        self.mp4 = mp4
+        self.resolutionStr = resolutionStr
+        self.src = src
+        self.rawUrls = rawUrls if rawUrls is not None else {}
 
 class AllAnime:
     def __init__(self):
-        #variables*
-        self.agent="Mozilla/5.0 (Windows NT 6.1; Win64; rv:109.0) Gecko/20100101 Firefox/109.0"
+        self.agent = "Mozilla/5.0 (Windows NT 6.1; Win64; rv:109.0) Gecko/20100101 Firefox/109.0"
         self.allanime_api = "https://api.allanime.day"
-        self.allanime_base="https://allanime.to"
+        self.allanime_base = "https://allanime.to"
+        self.lang = "en"
         self.mode = "sub"
-        self.quality = "best"
-        self.download_dir = "."
-        self.histfile = "ani-hsts"
-        self.skip = False
-        
-        # other
-        self.endpoint = requests.get(self.allanime_base + "/getVersion").json()["episodeIframeHead"]
-        
         self.internalLinks = [
             "Luf-mp4",
             "Sak",
-            # "Yt-mp4",
             "Default",
             "S-mp4",
-            "Ak"
         ]
+        self.endpoint = requests.get(self.allanime_base + "/getVersion").json()["episodeIframeHead"]
+        
+        self.cache_filename = "aniwatch.cache.json"
+        self.cache = self.load_cache()
         
         # queries
         self.popular_query = """
             query(
-                    $type: VaildPopularTypeEnumType!
-                    $size: Int!
-                    $page: Int
-                    $dateRange: Int
-                ) {
+                $type: VaildPopularTypeEnumType!
+                $size: Int!
+                $page: Int
+                $dateRange: Int
+            ) {
                 queryPopular(
                     type: $type
                     size: $size
@@ -76,7 +80,11 @@ class AllAnime:
                         name
                         thumbnail
                         englishName
+                        episodeCount
+                        score
+                        genres
                         slugTime
+                        __typename
                     }
                 }
             }
@@ -122,7 +130,24 @@ class AllAnime:
                 }
             }
         """
+        
     
+    def load_cache(self):
+        if os.path.exists(self.cache_filename):
+            with open(self.cache_filename, "r") as file:
+                return json.load(file)
+        return {}
+
+    def save_cache(self):
+        with open(self.cache_filename, "w") as file:
+            json.dump(self.cache, file, indent=4)
+
+    def get_from_cache(self, key):
+        return self.cache.get(key)
+
+    def add_to_cache(self, key, value):
+        self.cache[key] = value
+        self.save_cache()
     
     def decrypt(self, provider_id) -> str:
         decrypted = ''
@@ -137,6 +162,11 @@ class AllAnime:
         return link in self.internalLinks
     
     def get_popular(self, page:int):
+        cache_key = f"popular_{page}"
+        cached_data = self.get_from_cache(cache_key)
+        if cached_data:
+            if time.time() - cached_data["timestamp"] <= 60 * 60 * 24:
+                return cached_data
         resp = requests.get(
             f"{self.allanime_api}/api",
             params={
@@ -154,8 +184,11 @@ class AllAnime:
             }
         )
         
-        return resp.json()["data"]["queryPopular"]["recommendations"]
-    
+        data = resp.json()["data"]["queryPopular"]["recommendations"]
+        data["timestamp"] = time.time()
+        self.add_to_cache(cache_key, data)
+        return data
+
     def get_latest_update(self, page: int):
         resp = requests.get(
             f"{self.allanime_api}/api",
@@ -181,6 +214,13 @@ class AllAnime:
         return resp.json()["data"]["shows"]["edges"]
     
     def get_search(self, page: int, query: str):
+        cache_key = f"search_{query}"
+        cached_data = self.get_from_cache(cache_key)
+        if cached_data:
+            for edge in cached_data:
+                tmplist = []
+                tmplist.append(Anime(edge["_id"], edge["name"], int(edge["episodeCount"]), edge["thumbnail"], anime_type=edge["__typename"], anime_score=edge["score"],))
+                return tmplist
         resp = requests.get(
             f"{self.allanime_api}/api",
             params={
@@ -203,9 +243,20 @@ class AllAnime:
             }
         )
         
-        return resp.json()["data"]["shows"]["edges"]
+        data = resp.json()["data"]["shows"]["edges"]
+        self.add_to_cache(f"search_{query}", data)
+        anime_list = []
+        for edge in data:
+            anime_list.append(Anime(edge["_id"], edge["name"], int(edge["episodeCount"]), edge["thumbnail"], anime_type=edge["__typename"], anime_score=edge["score"],))
+            
+        return anime_list
     
     def get_anime_details(self, anime_id: str):
+        cache_key = f"anime_details_{anime_id}"
+        cached_data = self.get_from_cache(cache_key)
+        if cached_data:
+            return cached_data
+        
         resp = requests.get(
             f"{self.allanime_api}/api",
             params={
@@ -220,9 +271,15 @@ class AllAnime:
             }
         )
         
-        return resp.json()["data"]["show"]
+        data = resp.json()["data"]["show"]
+        self.add_to_cache(f"anime_details_{anime_id}", data)
+        return data
     
     def get_episodes_list(self, anime_id: str):
+        cache_key = f"episodes_list_{anime_id}"
+        cached_data = self.get_from_cache(cache_key)
+        if cached_data:
+            return cached_data
         resp = requests.get(
             f"{self.allanime_api}/api",
             params={
@@ -236,10 +293,15 @@ class AllAnime:
                 "User-Agent": self.agent
             }
         )
-        
-        return resp.json()["data"]["show"]["availableEpisodesDetail"][self.mode]
+        data = resp.json()["data"]["show"]["availableEpisodesDetail"][self.mode]
+        self.add_to_cache(f"episodes_list_{anime_id}", data)
+        return data
     
     def get_episode_streams(self, anime_id: str, episode_num: int) -> list:
+        cache_key = f"episode_streams{anime_id}_{episode_num}"
+        cached_data = self.get_from_cache(cache_key)
+        if cached_data:
+            return cached_data
         resp = requests.get(
             f"{self.allanime_api}/api",
             params={
@@ -256,34 +318,97 @@ class AllAnime:
             }
         )
         
-        return resp.json()["data"]["episode"]["sourceUrls"]
+        data = resp.json()["data"]["episode"]["sourceUrls"]
+        self.add_to_cache(f"episode_streams{anime_id}_{episode_num}", data)
+        return data
     
     def get_video_from_url(self, url: str, name: str) -> dict:
         decryptedUrl = self.decrypt(url.replace("--", ""))
-        print(decryptedUrl)
         resp = requests.get(self.endpoint + decryptedUrl.replace("/clock?", "/clock.json?"))
-        if resp.status_code!= 200:
+        if resp.status_code != 200:
             return None
         links = resp.json()["links"]
         return links
     
     def get_video_list(self, anime_id: str, episode_num: int):
+        cache_key = f"video_list_{anime_id}_{episode_num}"
+        cached_data = self.get_from_cache(cache_key)
+        if cached_data:
+            tmp_links = []
+            for video in tmp_links:
+                video = video[0]
+                link = video["link"]
+                hls = self.select(video, "hls", "mp4", reverse=True)
+                mp4 = self.select(video, "mp4", "hls", reverse=True)
+                resolution = video["resolutionStr"]
+                src = self.select(video, "src", "", opt2notInDict=True)
+                rawUrls = self.select(video, "rawUrls", {}, opt2notInDict=True)
+                video_links.append(VideoLink(link=link, hls=hls, mp4=mp4, resolutionStr=resolution, src=src, rawUrls=rawUrls))
+            return tmp_links
+        
+        
         episode_streams = self.get_episode_streams(anime_id, episode_num)
         video_list = []
         for stream in episode_streams:
             if self.isInternal(stream["sourceName"]):
-                print(f"{stream['sourceName']} is internal")
                 links = self.get_video_from_url(stream["sourceUrl"], stream["sourceName"])
-                print(links)
                 video_list.append(links)
-        
-        return video_list
+                
+        self.add_to_cache(f"video_list_{anime_id}_{episode_num}", video_list)
+                
+        video_links = []
+        for video in video_list:
+            video = video[0]
+            link = video["link"]
+            hls = self.select(video, "hls", "mp4", reverse=True)
+            mp4 = self.select(video, "mp4", "hls", reverse=True)
+            resolution = video["resolutionStr"]
+            src = self.select(video, "src", "", opt2notInDict=True)
+            rawUrls = self.select(video, "rawUrls", {}, opt2notInDict=True)
+            video_links.append(VideoLink(link=link, hls=hls, mp4=mp4, resolutionStr=resolution, src=src, rawUrls=rawUrls))
+        return video_links
     
+    def select(self, dictobj, opt1, opt2, reverse=False, opt2notInDict=False):
+        try:
+            val = dictobj[opt1]
+            return val
+        except KeyError:
+            if opt2notInDict:
+                return opt2
+            if reverse:
+                return not dictobj[opt2]
+            return dictobj[opt2]
 
+class Anime:
+    def __init__(self, anime_id: str, anime_title: str, episodes_count: int, thumbnail: str, description="", anime_type="", anime_status="", anime_score=0.0, anime_genres=[]):
+        self.id = anime_id
+        self.title = anime_title
+        self.episodes_count = episodes_count
+        self.episodes = []  # List to store fetched episodes
+        info = AllAnime().get_anime_details(self.id)
+        self.thumbnail = thumbnail or info["thumbnail"]
+        self.type = info["type"]
+        self.score = info["score"]
+        self.genres = info["genres"]
+        self.status = info["status"]
+        self.description = info["description"]
+    
+    def get_episode(self, episode_num: int):
+        # Fetch and append episode to self.episodes
+        episode_streams = AllAnime().get_video_list(self.id, episode_num)
+        episode_title = f"Episode {episode_num}"
+        self.episodes.append(Episode(self.id, episode_num, episode_title, episode_streams))
+
+class Episode:
+    def __init__(self, animeId: str, episode_num: int, episode_title: str, videoStreams: list):
+        self.num = episode_num
+        self.title = episode_title
+        self.streams = videoStreams
 
 def test():
     a = AllAnime()
-    print(a.get_video_list("vDTSJHSpYnrkZnAvG", 1))
-    # AllAnime().get_video_from_url("175948514e4c4f57175b54575b5307515c050f5c0a0c0f0b0f0c0e590a0c0b5b0a0c0a010e5a0e0b0e0a0e5e0e0f0b0d0a010f080e5e0e0a0e0b0e010f0d0a010f080c0a0d0a0d0d0c5b0c5d0d0d0f0e0d5e0e000f0c0e5c0d5b0e000c0f0f080c090a010f0d0f0b0e0c0a010b0f0a0c0a590a0c0f0d0f0a0f0c0e0b0e0f0e5a0e0b0f0c0c5e0e0a0a0c0b5b0a0c0c0a0f0c0e010f0e0e0c0e010f5d0a0c0a590a0c0e0a0e0f0f0a0e0b0a0c0b5b0a0c0b0c0b0e0b0c0b0a0a5a0b0e0b0a0a5a0b0c0b0f0d0a0b0c0b0c0b5b0b0f0b0e0b5b0b0e0b0e0a000b0e0b0e0b0e0d5b0a0c0a590a0c0f0a0f0c0e0f0e000f0d0e590e0f0f0a0e5e0e010e000d0a0f5e0f0e0e0b0a0c0b5b0a0c0f0d0f0b0e0c0a0c0a590a0c0e5c0e0b0f5e0a0c0b5b0a0c0e0b0f0e0a5a0a010e5a0e0b0e0a0e5e0e0f0b0d0a010f080e5e0e0a0e0b0e010f0d0a010f080c0a0d0a0d0d0c5b0c5d0d0d0f0e0d5e0e000f0c0e5c0d5b0e000c0f0f080c090a010f0d0f0b0e0c0a010b0f0a0c0f5a", "Luf-mp4")
+    anime = a.get_search(1, "KnY")[0]
+    anime.get_episode(1)  # Fetch and append episode 1
+    print(anime.episodes[0].title)  # Print title of the first episode
 
 test()
